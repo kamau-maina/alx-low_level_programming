3a4aa29807cb721ceeec6ef6409d81adf2a455f9
kamau maina <mainkamjames@gmail.com>
2022-09-08 08:00:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
-int main(void){
- puts("\"Programming is like building a multilingual puzzle");
+/*
+ * File: 4-puts.c
+ * Auth: Kamau Maina <mainkamjames@gmail.com>
+ */
 
-return 0;
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
