a98edc4c9813a8f5f3f05048e72b04ceeaf1582e
kamau maina <mainkamjames@gmail.com>
2022-09-08 08:19:46 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,19 @@
 /*
-  * File: 5-printf.c
-  * Auth: Kamau Maina 
-  */
+ * File: 5-printf.c
+ * Auth: Kamau Maina
+ */
+
 #include <stdio.h>
+
 /**
- * main - prints "with proper grammar, but the 
- *          outcome is a piece of art,"followed by a new line.
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
  * Return: Always 0.
  */
 int main(void)
 {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+	return (0);
 }
- 
