38a764dc871e336bf5cd6e2ad244c206b1c8d941
kamau maina <mainkamjames@gmail.com>
2022-09-10 04:43:56 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,8 +6,9 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of three different digits, in ascending order.
- * 
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -22,18 +23,18 @@ int main(void)
 			{
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) +'0');
+				putchar((digit3 % 10) + '0');
+
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
+				
 				putchar(',');
 				putchar(' ');
-                           }             
 			}
-		}		
+		}
+	}
 
 	putchar('\n');
-	return 0;
 
-
-
+	return (0);
 }
