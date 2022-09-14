ce8c964b7c489978885d52fa9b34bdc5d81a8e2c
kamau maina <mainkamjames@gmail.com>
2022-09-14 14:37:56 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,11 @@
 
 
 #include "main.h"
-void print_alphabet_x10(void);
+/**
+ * print_alphabet_x10(void)
+ */
+
+void print_alphabet_x10(void)
 {       
         
         for (int i = 0; i<=9; i++)
