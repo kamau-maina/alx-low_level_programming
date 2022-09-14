4ddc5730e64f126df944ab54a36a9c15dc6c329d
kamau maina <mainkamjames@gmail.com>
2022-09-14 16:05:13 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+/*
+ * File:8-24_hours.c
+ * Auth:Kamau Maina
+ */
+
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of jack bauer, starting from 00:00 to 23:59
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
