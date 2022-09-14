9f4c022cc5bb68b8c243a4d61550c3f3283def18
kamau maina <mainkamjames@gmail.com>
2022-09-14 16:52:24 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,25 @@
  */
 void times_table(void)
 {
-	int a;
-	int b;
-	int c;
+	int a, b, c;
+
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 10; b++)
+		_putchar('0');
+
+		for (b = 1; b <= 9; b++)
 		{
-			c=a*b;
-			_putchar((c/10) + '0');
+			_putchar(',');
 			_putchar(' ');
+
+			c = a * b;
+
+			if (c <= 9)
+				_putchar(' ');
+			else
+				_putchar((c / 10) + '0');
+
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 	}
