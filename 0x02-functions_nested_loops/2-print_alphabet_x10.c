1133e2e0f7a437e1c18dc3b53c056f7cc9ac80c0
kamau maina <mainkamjames@gmail.com>
2022-09-14 14:48:02 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,27 +3,27 @@
  * Auth: Kamau Maina
  */
 
-
 #include "main.h"
+
 /**
- * print_alphabet_x10(void)
+ * print_alphabet_x10(void - prints the alphabet in lower case 10 times)
  */
 
 void print_alphabet_x10(void)
 {       
 	int i;
 	char letter;	
-        for (i = 0; i <= 9; i++)
-        {
-                for (letter = 'a'; letter <= 'z'; letter++)
+	for (i = 0; i <= 9; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{		
 
-                        _putchar(letter);
+			_putchar(letter);
                 
 		}
 
 		_putchar('\n');
 
-        }       
+	}
         
 }
