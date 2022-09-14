84cdf09faacaf0498b18e850a79775c0eb5da941
kamau maina <mainkamjames@gmail.com>
2022-09-14 14:52:44 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,20 +10,21 @@
  */
 
 void print_alphabet_x10(void)
-{       
+{
 	int i;
-	char letter;	
+	char letter;
+	
 	for (i = 0; i <= 9; i++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
-		{		
+		{
 
 			_putchar(letter);
-                
+
 		}
 
 		_putchar('\n');
 
 	}
-        
+
 }
