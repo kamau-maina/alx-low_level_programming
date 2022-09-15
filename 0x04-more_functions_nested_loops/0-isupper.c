cdf4c6fa64265b6ce8cd18cae04d3b86bb1dc377
kamau maina <mainkamjames@gmail.com>
2022-09-15 11:21:43 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0-isupper.c.
+ * Auth: Kamau Maina.
+ */
+
+#include "main.h"
+
+/*
+ * isupper - checks for uppercase character.
+ * Return: 1 if uppercase, 0 otherwise.
+ * @c: the character to be checked.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+
+
+}
