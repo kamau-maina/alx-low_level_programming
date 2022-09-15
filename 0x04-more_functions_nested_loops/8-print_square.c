2960bfd07acfe8e37718f246a8aa036b7a8b065a
kamau maina <mainkamjames@gmail.com>
2022-09-15 16:04:19 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+/*
+ * File: 8-print_square.c
+ * Auth: Kamau Maina
+ */
+
+#include "main.h"
+
+/**
+ * void print_square - prints a square followed by a new line
+ * @size:holds the dimensions of the square
+ */
+
+void print_square(int size)
+{
+	int length, width;
+
+	for (length = 0; length < size; length++)
+	{
+		for (width = 0; width < size; width++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+
+
