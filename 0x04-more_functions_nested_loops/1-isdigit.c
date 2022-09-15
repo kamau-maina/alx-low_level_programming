4beb6edcaed53f707cd3143ba398c3aaf8aa33a6
kamau maina <mainkamjames@gmail.com>
2022-09-15 12:15:40 -0700

    1-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+/*
+ * File: 1-isdigit.c
+ * Auth: Kamau Maina
+ */
+
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit o through 9.
+ *@c: the digit to be checked.
+ *Return: 1 if c is a digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+
+}
