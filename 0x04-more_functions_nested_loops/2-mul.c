ea6aa52686a00b8743d7cdac117fa8a334457126
kamau maina <mainkamjames@gmail.com>
2022-09-15 12:27:09 -0700

    function to find the product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+ * File: mul.c
+ * Auth: Kamau Maina
+ */
+
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ * @a: first integer.
+ * @b:second integer.
+ *Return: the product of the two integers.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
