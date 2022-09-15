18b2387c986bac475aaabbc8620d9aaae7c3462f
kamau maina <mainkamjames@gmail.com>
2022-09-14 17:01:39 -0700

    10-sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/*
+ * File: 10-add.c
+ * Auth: Kamau Maina
+ */
+
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * Return: sum of the two integers
+ */
+int add(int,int)
+{
+	int a,b,sum;
+	sum=a+b;
+	return (sum);
+}
