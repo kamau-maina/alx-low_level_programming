bb5b066042152c193f7281c56f7c9569ac714293
kamau maina <mainkamjames@gmail.com>
2022-09-20 10:21:36 -0700

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+/*
+ * File: 4-print_rev.c
+ * Auth: Kamau Maina
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void print_rev(char *s)
+{
+	int length = 0, index;
+
+	while (s[index++])
+		length++;
+
+	for (index = length - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
+}
