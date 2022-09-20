a807b44ceba90989162e207f560521b77e2e4ee1
kamau maina <mainkamjames@gmail.com>
2022-09-20 10:11:42 -0700

    check the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Kamau Maina
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the character array to be checked
+ * Return: The string to check the length of
+ */
+
+int _strlen(const char *s)
+{
+	size_t length = 0;
+
+	while (*s++)
+		length++;
+
+	return (length);
+}
