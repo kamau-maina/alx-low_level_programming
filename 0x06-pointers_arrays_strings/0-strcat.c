981f02e25bf741705d36807729768fc6f9f71206
kamau maina <mainkamjames@gmail.com>
2022-09-22 15:15:54 -0700

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,13 +15,13 @@
 
 char *_strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	int index = 0, dest_length = 0;
 
 	while (dest[index++])
-		dest_len++;
+		dest_length++;
 
 	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+		dest[dest_length++] = src[index];
 
 	return (dest);
 }
