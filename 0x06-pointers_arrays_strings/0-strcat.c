dcce54dd984cb087c5d0c79a88ccff72c1545578
kamau maina <mainkamjames@gmail.com>
2022-09-22 15:13:53 -0700

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,18 @@
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
- * Return: A pointer to the destination string @dest.
+ * Return: A pointer to the destination string, @dest.
  */
-char *_strcat(char *dest, const char *src)
+
+char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_length = 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-		dest_length++;
+		dest_len++;
 
 	for (index = 0; src[index]; index++)
-		dest[dest_length++] = src[index];
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
