83e5c4b4d2e47f42f6d77706df742e9de06a1fc6
kamau maina <mainkamjames@gmail.com>
2022-09-22 15:20:51 -0700

    encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+/*
+ * File: 7-leet.c
+ * Auth: Kamau Maina
+ */
+
+#include "main.h"
+
+/**
+ * leet - Encodes a string to 1337.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *leet(char *str)
+{
+	int index = 0, indx1;
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+
+	while (str[index])
+	{
+		for (index1 = 0; index1 <= 7; index1++)
+		{
+			if (str[index] == leet[index1] ||
+			    str[index] - 32 == leet[index1])
+				str[index] = index1 + '0';
+		}
+
+		index++;
+	}
+
+	return (str);
+}
