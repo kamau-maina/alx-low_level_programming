262465a9973ab40c9a4e0fa40000f5aaccb40845
kamau maina <mainkamjames@gmail.com>
2022-09-27 04:16:52 -0700

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+/*
+ * File: 2-strchr.c
+ * Auth: Kamau Maina
+ */
+
+#include "main.h"
+
+/*
+ * *_strchr - returns a pointer to the first occurrence
+ * 		of a character @c in a string @s.
+ *@s: the string to be checked
+ *@c: the character to search for
+ *Return: a pointer to c's first occurrence
+ *		 if c is found and null
+ *		 if c is not found
+ */
+
+char *_strchr(char *s, char c)
+{
+		int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return ('\0');
+
+
+}
