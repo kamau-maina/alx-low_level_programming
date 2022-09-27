d6e574de39f9e941f50bf258f20dfaa9542ee686
kamau maina <mainkamjames@gmail.com>
2022-09-27 03:45:02 -0700

    fills memory with a constant byte

---
+++ 0x07-pointers_arrays_strings/0-memeset.c
@@ -0,0 +1,25 @@
+/*
+ * File: 0-memset.c
+ * Auth: Kamau Maina
+ */
+
+#include "main.h"
+
+/**
+ * memset - Fills memory with a constant byte.
+ * @s: A pointer to the memory area.
+ * @c: The character to fill the memory area with.
+ * @n: The number of bytes to be filled.
+ *
+ * Return: A pointer to the filled memory area @s.
+ */
+void *_memset(void *s, int c, size_t n)
+{
+	unsigned int i;
+	unsigned char *memory = s, value = c;
+
+	for (i = 0; i < n; i++)
+		memory[i] = value;
+
+	return (memory);
+}
