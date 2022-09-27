a5a99fbfb5e84b93ae4c7c4073ca75aa31171f32
kamau maina <mainkamjames@gmail.com>
2022-09-27 03:51:26 -0700

    added desc for return value

--- 0x07-pointers_arrays_strings/0-memeset.c
+++ 0x07-pointers_arrays_strings/0-memeset.c
@@ -11,7 +11,7 @@
  * @c: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
  *
- * Return: A pointer to the filled memory area @s.
+ * Return: A pointer to the filled memory area .
  */
 void *_memset(void *s, int c, size_t n)
 {
