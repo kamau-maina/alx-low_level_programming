bedc2f05865bfa8fdd4cc63dfa2add296acb6c2c
kamau maina <mainkamjames@gmail.com>
2022-09-29 03:02:42 -0700

    counts the number of characters in a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,25 @@
+/*
+ * File: 2-strlen_recursion.c
+ * Auth: Kamau Maina
+ */
+
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of a string.
+ * @s: the string whose length to print.
+ * Return: nothing.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int index;
+
+	count = 0;
+	if (*s)
+	{
+		count++;
+		count += _strlen_recursion(s + 1);
+	}
+	return count;
+}
