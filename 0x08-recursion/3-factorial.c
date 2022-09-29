80966fe15585ccdf2e633715a34da72ae12673d8
kamau maina <mainkamjames@gmail.com>
2022-09-29 03:34:10 -0700

    find factorial of a given number.

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,28 @@
+/*
+ * File: 3-factorial.c
+ * Auth: Kamau Maina
+ */
+
+#include "main.h"
+
+/**
+ * factorial - finds the factorial of a given number.
+ * @n: the number whose factorial to find.
+ *Return: factoria if @n >0, -1 otherwise.
+ */
+
+int factorial(int n)
+{
+	if (n > 1)
+	{
+		return (factorial(n - 1));
+	}
+	else if (n == 1 || n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+}
