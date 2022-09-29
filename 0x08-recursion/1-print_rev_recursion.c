c21bfa11473dff37aedf79c2b1e4655abb7d5dc7
kamau maina <mainkamjames@gmail.com>
2022-09-29 02:35:37 -0700

    reverse a string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+/*
+ * File: 1-print_rev_recursion.c
+ * Auth:Kamau Maina
+ */
+
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * Return: nothing
+ * @s:the string to be reversed.
+ */
+ 
+ void _print_rev_recursion(char *s)
+ {
+	 if (*s)
+	 {
+		 _print_rev_recursions(s + 1);
+		 _putchar(*s);
+	 }
+ }
+
