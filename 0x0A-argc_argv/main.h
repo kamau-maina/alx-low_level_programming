8cb564e0e78ad42fbfe88a137a6617c3a5152d56
kamau maina <mainkamjames@gmail.com>
2022-10-01 13:14:50 -0700

    function prototypes

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef main_H
+#define main_H
+
+/**
+ * File: main.h
+ * Auth: Kamau Maina
+ * Desc:Header file containing function prototypes
+ * 	for functions in th 0x0A dir.
+ */
+
+int putchar(char c);
