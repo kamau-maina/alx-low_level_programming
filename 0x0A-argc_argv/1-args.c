1b3773131785f1aac8fb327accaff79eb4e81934
kamau maina <mainkamjames@gmail.com>
2022-10-03 01:59:50 -0700

    number of command line arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+/*
+ * File: 1-args.c
+ * Auth: Kamau Maina
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to it.
+ * @argc: the number of arguments passed to main.
+ * @argv: pointers array to the arguments.
+ * Return: 0.
+ */
+
+int main(int argc, char __attribute__((__unused__)) **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
