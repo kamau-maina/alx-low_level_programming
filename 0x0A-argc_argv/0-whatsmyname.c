2ba01e1e6eeb2d9ba138f8b18955491c77193330
kamau maina <mainkamjames@gmail.com>
2022-10-03 01:38:58 -0700

    added desc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,6 +3,17 @@
  * Auth: Kamau Maina
  */
 #include <stdio.h>
+
+/**
+ * main - prints the program name followed by a new line
+ * @argc: the number of command line arguments
+ * 	  given to the program
+ * @argv: an array of pointers to the arguments
+ *Return: always 0.
+ */
+
+
+
 int main(int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
