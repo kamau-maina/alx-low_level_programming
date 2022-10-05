54cedfd900256db4adbb3885bd93668b4b1f1f3b
kamau maina <mainkamjames@gmail.com>
2022-10-05 05:55:01 -0700

    2-d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	grid = malloc(sizeof(int *) * height)
+	grid = malloc(sizeof(int *) * height);
 	
 	if (grid == NULL)
 		return (NULL);
