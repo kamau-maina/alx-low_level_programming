819955de0ac6901e9aac0e2b7e4c8e50cbd58059
kamau maina <mainkamjames@gmail.com>
2022-10-05 05:25:00 -0700

    fixed null string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,12 +21,12 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = " ";
+		s1 = "";
 	}
 
 	if (s2 == NULL)
 	{
-		s2 = " ";
+		s2 = "";
 	}
 
 	for (index = 0; s1[index] || s2[index]; index++)
