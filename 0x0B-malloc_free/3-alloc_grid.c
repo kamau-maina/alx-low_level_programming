b26757a56c381e5c06b03e567d36f3025450a98a
kamau maina <mainkamjames@gmail.com>
2022-10-05 06:02:51 -0700

    removed trailing whitespace

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,15 +9,15 @@
 /**
  * alloc_grid - returns a pointer to a 2 dimensional integers aaray.
  * @width: the width of the grid.
- * @height: height of the grid. 
+ * @height: height of the grid.
  * Return: pointer to a 2 dim array.
- * 	   NULL on failure.
- * 	   NULL if width or height is 0 or negative.
+ *	   NULL on failure.
+ *	   NULL if width or height is 0 or negative.
  */
 
 int **alloc_grid(int width, int height)
 {
-	int w,h;
+	int w, h;
 	int **grid;
 
 	if (width < 1 || height < 1)
@@ -26,17 +26,17 @@ int **alloc_grid(int width, int height)
 	}
 
 	grid = malloc(sizeof(int *) * height);
-	
+
 	if (grid == NULL)
 		return (NULL);
 
 	for (h = 0; h < height; h++)
 	{
 		grid[h] = malloc(sizeof(int) * width);
-		
+
 		if (grid[h] == NULL)
 		{
-			for (; h>= 0; h--)
+			for (; h >= 0; h--)
 				free(grid[h]);
 			free(grid);
 			return (NULL);
