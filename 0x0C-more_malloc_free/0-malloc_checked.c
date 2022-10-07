32a46559524abd68e8fac94a7422e158bc0830de
kamau maina <mainkamjames@gmail.com>
2022-10-06 22:59:10 -0700

    added desc for the function

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,6 +6,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to be allocated.
+ *
+ * Return: A pointer to the allocated memory.
+ */
+
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
