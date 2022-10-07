2ff62ec2e59c72f9aaac9d3b53c7814baff36a76
kamau maina <mainkamjames@gmail.com>
2022-10-06 23:30:01 -0700

    chaged len from int to unsigned int type

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,37 +17,30 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len = n, index;
 	char *concat_str;
+	unsigned int len = n, index;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
-		s2 == "";
-	}
+		s2 = "";
 
 	for (index = 0; s1[index]; index++)
-	{
 		len++;
-	}
 
 	concat_str = malloc(sizeof(char) * (len + 1));
 
 	if (concat_str == NULL)
-	{
 		return (NULL);
-	}
 
 	len = 0;
 
-	for (index = 0; s2[index]; index++)
-	{
+	for (index = 0; s1[index]; index++)
+		concat_str[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
 		concat_str[len++] = s2[index];
-	}
 
 	concat_str[len] = '\0';
 
