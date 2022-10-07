9f178d3ebdc1b81867d7a5ef7492a031b3d203cb
kamau maina <mainkamjames@gmail.com>
2022-10-07 00:06:38 -0700

    array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,44 @@
+/*
+ * File: 2-calloc.c.
+ * Auth: Kamau Maina.
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ * _calloc - allocates memory for an array using malloc.
+ * @nmemb: The number of elements of the array.
+ * @size: The byte size of each array element.
+ * Return : Null if nmemb or size = 0 or if func fails.
+ *	   otherwise, a pointerto the allocated mem space.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	char *fill;
+	unsigned int index;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	fill = ptr;
+
+	for (index = 0; index < (size * nmemb); index++)
+	{
+		fill[index] = '0';
+	}
+
+	return (ptr);
+}
+
