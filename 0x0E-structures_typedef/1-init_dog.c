12f69944fd6d11634f11d9f845d82756fb67266a
kamau maina <mainkamjames@gmail.com>
2022-10-12 05:11:00 -0700

    corrected errors

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -18,9 +18,9 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		my_dog.name = name;
-		my_dog.age = age;
-		my_dog.owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -28,8 +28,5 @@ struct dog
 
 typedef struct dog my_dog;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
