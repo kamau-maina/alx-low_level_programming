47203eedbc9c0eccb5ec90831f457e7bb94cb018
kamau maina <mainkamjames@gmail.com>
2022-10-12 08:55:16 -0700

    doug

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,29 +1,88 @@
 /*
- * File: 4-new_dog.c.
- * Auth: Kamau Maina.
+ * File: 4-new_dog.c
+ * Auth: Kamau Maina
  */
 
 #include "dog.h"
+#include <stdlib.h>
+
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
 
 /**
- * new_dog - creates a new dog.
- * @name: name of the new dog.
- * @age: age of the new dog.
- * @owner: owner of the new dog.
- * Return: NULL if the function fails.
+ * _strlen - Finds the length of a string.
+ * @str: The string to be measured.
+ *
+ * Return: The length of the string.
  */
+int _strlen(char *str)
+{
+	int len = 0;
 
+	while (*str++)
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strcopy - Copies a string pointed to by src, including the
+ *            terminating null byte, to a buffer pointed to by dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ *
+ * Return: The pointer to dest.
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: The new struct dog.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *my_dog;
+	dog_t *doug;
 
 	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	doug = malloc(sizeof(dog_t));
+	if (doug == NULL)
+		return (NULL);
+
+	doug->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (doug->name == NULL)
 	{
+		free(doug);
 		return (NULL);
 	}
 
-	*my_dog = malloc(sizeof(dog_t));
-	if (*my_dog == NULL)
+	doug->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doug->owner == NULL)
+	{
+		free(doug->name);
+		free(doug);
 		return (NULL);
+	}
 
+	doug->name = _strcopy(doug->name, name);
+	doug->age = age;
+	doug->owner = _strcopy(doug->owner, owner);
 
+	return (doug);
+}
