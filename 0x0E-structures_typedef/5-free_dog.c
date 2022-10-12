e0e1d6c3343b451e18f4b74d2416d1588e768490
kamau-maina <mainkamjames@gmail.com>
2022-10-12 13:12:13 -0700

    freegit add ../

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -4,18 +4,18 @@
  */
 
 #include "dog.h"
-#include <stdlib.h>
 
 /**
- * free_dog - Frees dogs.
- * @d: The dog to be freed.
+ * free_dog - frees dogs.
+ * @d: struct dog.
+ *
  */
 void free_dog(dog_t *d)
 {
-	if (d == NULL)
-		return
-
-	free(d->owner);
-	free(d->name);
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
