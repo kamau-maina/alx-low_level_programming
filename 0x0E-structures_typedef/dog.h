a49e9ed0aaa241cdb943b335964b4ed5feb75922
kamau maina <mainkamjames@gmail.com>
2022-10-12 05:50:43 -0700

    defined a new type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,8 +26,7 @@ struct dog
  * my_dog - defined type for struct dog.
  */
 
-typedef struct dog my_dog;
-typedef dog dot_t
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
