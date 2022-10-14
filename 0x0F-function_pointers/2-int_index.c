63efbf371db9c839129d290bb32d3aef66ee42a2
kamau-maina <mainkamjames@gmail.com>
2022-10-14 09:54:01 -0700

    search

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+/*
+ * File: 2-int_index.c
+ * Auth: Kamau Maina
+ */
+
+#include "function_pointers.h"
+
+/**
+ * int_indes - searches for an integer
+ * @size: the number of elements in the array
+ * @cmp: pointer to the function to be used to
+ * 	 compare values.
+ * @array: the array of integers.
+ * Return: index to the first element for which
+ * 	   cmp does not return 0.
+ * 	   -1 if no elements match.
+ * 	   -1 if size < 0.
+ */
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (index = 0; index < size; index++)
+	{
+		if (cmp(array[index]) != 0)
+			return (index);
+	}
+
+	return (-1);
+}
