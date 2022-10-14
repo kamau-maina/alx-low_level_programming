9c1895434b36a3662d63fb483116aad4b75d0098
kamau-maina <mainkamjames@gmail.com>
2022-10-14 09:41:29 -0700

    added desc for array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,8 @@
  * array_iterator - executes a function given as a
  *		    parameter on each element of an array
  *@size: size of the array
- *action: a pointer to the function you need to use
+ *@array: the array.
+ * @action: a pointer to the function you need to use
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
