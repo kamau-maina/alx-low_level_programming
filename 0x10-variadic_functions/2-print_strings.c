83ff334ac1bac8f2442034dcbab41ddd36a8f837
kamau-maina <mainkamjames@gmail.com>
2022-10-15 14:02:58 -0700

    print_strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+/*
+ * File: 2-print strings
+ * Auth: Kamau Maina
+ */
+
+#include "variadic_functions"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_strings - prints a string followed by a new line.
+ * @separator: the string to be printed between the strings.
+ * @n:number of strings passed to the function.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int index = 0;
+	char *c;
+	va_list str;
+
+	va_start(str, n);
+	
+	for (; index < n; index++)
+	{
+		c = va_arg(str, char *);
+
+		if (c == NULL)
+			printf("(nill)");
+
+		else
+			printf("%s",c);
+
+		if (index != (n-1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(str);
+}
