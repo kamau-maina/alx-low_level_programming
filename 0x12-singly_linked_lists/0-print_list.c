c7f1a34c57292615fad463938e1b9402db6e31b8
kamau maina <mainkamjames@gmail.com>
2022-10-25 12:57:57 -0700

    cleared errors

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,17 +17,19 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
+		{
+			printf("[%u] %s\n", h->len, "(nil)");
+		}
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
 		h = h->next;
-	}
+		nodes++;
 
+	}
 	return (nodes);
 }
