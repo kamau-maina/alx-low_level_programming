0fb3a403b1763f78f00b6affe9797f7a22f55ba3
kamau maina <mainkamjames@gmail.com>
2022-10-25 13:11:14 -0700

    removed trailing whitespace

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,10 +21,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t; 
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 
 
