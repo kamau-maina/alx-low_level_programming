53a250415f9c7c6c3736877aca0216648e406c07
kamau maina <mainkamjames@gmail.com>
2022-10-25 08:31:57 -0700

    added desc for structure

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 
 /**
- * struct list_t - singly linked list
+ * struct list_s - singly linked list
  * @str: string
  * @len: length of the string
  * @next: points to the next node
@@ -24,13 +24,13 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_t *next;
-}list_t;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
+}
 #endif
 
