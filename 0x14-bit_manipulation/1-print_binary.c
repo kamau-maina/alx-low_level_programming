d1e44b12024917b40c32244ee0372f43d0610d4d
kamau maina <mainkamjames@gmail.com>
2022-11-01 05:16:18 -0700

    completed task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,7 +3,7 @@
  * Auth: Kamau Maina
  */
 
-#incude "main.h"
+#include "main.h"
 
 /**
  * print_binary - prints the binary representation of a number.
@@ -15,5 +15,5 @@ void print_binary(unsigned long int n)
 	if (n > 1)
 		print_binary(n >> 1);
 
-	_putchar((n & 1) + '0'));
+	_putchar((n & 1) + '0');
 }
