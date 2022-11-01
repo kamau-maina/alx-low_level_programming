c8487f13d2cf7622e20db15cce235858e9163651
kamau maina <mainkamjames@gmail.com>
2022-11-01 05:02:05 -0700

    dec - bin

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+/*
+ * File: 1-print_binary.c
+ * Auth: Kamau Maina
+ */
+
+#incude "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: the number to be converted to binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0'));
+}
