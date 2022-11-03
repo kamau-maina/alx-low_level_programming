3e8b47f80c746913d5f7068db5e3aa0abb90cbb9
kamau maina <mainkamjames@gmail.com>
2022-11-03 14:42:28 -0700

    completed tasks

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+
+/*
+ * File: 0-read_textfile.c
+ * Auth: Kamau Maina
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: 0 if filename is null.
+ *               write fails or does not write the expecte ammount
+ *               of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t op, re, wr;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	re = read(op, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, re);
+
+	if (op == -1 || re == -1 || wr == -1 || wr != re)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(op);
+
+	return (wr);
+}
